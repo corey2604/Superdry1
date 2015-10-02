8a5381fb9c96799dcbb352a80f480119154abb81
Kainos User <c.duffy@kainos>
2015-10-02 15:42:07 +0100

    System allows for withdrawals

--- src/Account.java
+++ src/Account.java
@@ -13,19 +13,22 @@ public class Account {
     String Surname;
     AccountType AccountT;
     float Savings;
+    float OverDraftLimit;
 
-    public Account (int AccountNo, String FirstName,String Surname, AccountType AccountT){
+    public Account (int AccountNo, String FirstName,String Surname, AccountType AccountT, int OverDraftLimit){
         this.AccountNo=AccountNo;
         this.FirstName=FirstName;
         this.Surname=Surname;
         this.AccountT=AccountT;
+        this.OverDraftLimit=OverDraftLimit;
     }
-    public Account (int AccountNo, String FirstName,String Surname, AccountType AccountT, float Savings){
+    public Account (int AccountNo, String FirstName,String Surname, AccountType AccountT, float Savings,float OverDraftLimit){
         this.AccountNo=AccountNo;
         this.FirstName=FirstName;
         this.Surname=Surname;
         this.AccountT=AccountT;
         this.Savings=Savings;
+        this.OverDraftLimit=OverDraftLimit;
     }
 
     public static String getAccountDetails(int AccountNo, String FirstName, String Surname, AccountType AccountT, float Savings){
@@ -40,8 +43,19 @@ public class Account {
        return Savings;
     }
 
-    /*public void getWithdrawal(int WithdrawalAmount) throws Exception
-        if(Withdrawal>=) */
+    public float addFunds(float DepInput) {
+        return Savings+DepInput;
+    }
+
+    public void withdraw(float WithdrawalAmount) throws Exception{
+        if(WithdrawalAmount<=(Savings+OverDraftLimit)){
+           System.out.println("Funds removed.");
+           Savings-=WithdrawalAmount;
+           System.out.println("Your funds are currently: "+Savings);
+            Main.WithdrawCheck=true;
+        }
+        else throw new AccountWithdrawalException("Insufficient funds");
+    }
 
    }
 
