72600954b110cb157816922cae5d4554de89fef9
Kainos User <c.duffy@kainos>
2015-10-06 15:48:50 +0100

    BankDB SQL structure added

---
+++ src/SQLConnector.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: kuser
+ * Date: 06/10/15
+ * Time: 14:21
+ * To change this template use File | Settings | File Templates.
+ */
+public class SQLConnector {
+}
