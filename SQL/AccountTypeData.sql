dd06ad908b7541157ce18a3ee9e0f8e928b08dc9
Kainos User <c.duffy@kainos>
2015-10-08 11:53:20 +0100

    Work done to insert statements

---
+++ SQL/AccountTypeData.sql
@@ -0,0 +1,3 @@
+insert into AccountType(AccountType, OverdraftLimit, DeductionCharge) values("Standard", 500, 0);
+insert into AccountType(AccountType, OverdraftLimit, DeductionCharge) values("Premium", 3000, 0);
+insert into AccountType(AccountType, OverdraftLimit, DeductionCharge) values("Saver", 0, 1);
